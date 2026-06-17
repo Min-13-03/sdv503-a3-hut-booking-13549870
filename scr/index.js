065eb0abef2d18e99729a7580ef6382012eb28b1
Nazmin Kata <nazminnisha5424@gmail.com>
2026-06-18 06:35:24 +1200

    Import all the source to the main branch

---
+++ scr/index.js
@@ -0,0 +1,4 @@
+import { loadBookings } from "./data.js";
+import { createBooking, cancelBooking } from "./booking.js";
+import { occupancySummary } from "./summary.js";
+import { ask } from "./command.js";
